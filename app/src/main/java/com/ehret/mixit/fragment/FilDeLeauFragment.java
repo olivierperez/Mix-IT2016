d0c996dbd31f8864acc38c70e326bbf887bce36b
Olivier PEREZ <olivier@olivierperez.fr>
2017-04-18 23:03:21 +0200

    Improve performance + perf feeling of FilDeLeauFragment

--- app/src/main/java/com/ehret/mixit/fragment/FilDeLeauFragment.java
+++ app/src/main/java/com/ehret/mixit/fragment/FilDeLeauFragment.java
@@ -1,13 +1,14 @@
 package com.ehret.mixit.fragment;
 
-import android.app.Activity;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.ehret.mixit.HomeActivity;
@@ -17,25 +18,28 @@ import com.ehret.mixit.domain.TypeFile;
 import com.ehret.mixit.domain.talk.Talk;
 import com.ehret.mixit.model.ConferenceFacade;
 
+import java.util.List;
+
 
 public class FilDeLeauFragment extends Fragment {
 
-    private ListView liste;
+    private ListView talksListView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_datalist, container, false);
-
-        //Handle with layout
-        this.liste = (ListView) rootView.findViewById(R.id.liste_content);
-
-        return rootView;
+        return inflater.inflate(R.layout.fragment_datalist, container, false);
     }
 
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        ((HomeActivity) activity).onSectionAttached("title_fildeleau","color_primary");
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        talksListView = (ListView) view.findViewById(R.id.liste_content);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        ((HomeActivity) context).onSectionAttached("title_fildeleau","color_primary");
     }
 
     /**
@@ -44,30 +48,49 @@ public class FilDeLeauFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        Context context = getActivity().getBaseContext();
-        liste.setClickable(true);
+        Context context = getContext();
 
-        liste.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Talk conf = (Talk) liste.getItemAtPosition(position);
+        talksListView.setOnItemClickListener((parent, view, position, id) -> {
+            Talk conf = (Talk) talksListView.getItemAtPosition(position);
 
-                if("Special".equals(conf.getFormat())){
-                    ((HomeActivity) getActivity()).changeCurrentFragment(
-                            SessionDetailFragment.newInstance(TypeFile.special.toString(), conf.getIdSession(),-1), TypeFile.special.toString());
-                } else if("WORKSHOP".equals(conf.getFormat())){
-                    ((HomeActivity) getActivity()).changeCurrentFragment(
-                            SessionDetailFragment.newInstance(TypeFile.workshops.toString(), conf.getIdSession(),4), TypeFile.workshops.toString());
-                } else {
-                    ((HomeActivity) getActivity()).changeCurrentFragment(
-                            SessionDetailFragment.newInstance(TypeFile.talks.toString(), conf.getIdSession(),3), TypeFile.talks.toString());
-                }
+            if("Special".equals(conf.getFormat())){
+                ((HomeActivity) getActivity()).changeCurrentFragment(
+                        SessionDetailFragment.newInstance(TypeFile.special.toString(), conf.getIdSession(),-1), TypeFile.special.toString());
+            } else if("WORKSHOP".equals(conf.getFormat())){
+                ((HomeActivity) getActivity()).changeCurrentFragment(
+                        SessionDetailFragment.newInstance(TypeFile.workshops.toString(), conf.getIdSession(),4), TypeFile.workshops.toString());
+            } else {
+                ((HomeActivity) getActivity()).changeCurrentFragment(
+                        SessionDetailFragment.newInstance(TypeFile.talks.toString(), conf.getIdSession(),3), TypeFile.talks.toString());
             }
         });
 
-        ListTalkForFilAdapter adapter = new ListTalkForFilAdapter(context, ConferenceFacade.getInstance().getWorkshopsAndTalks(context));
-        liste.setAdapter(adapter);
+        if (talksListView.getAdapter() == null) {
+            new Loading(context).execute();
+        }
+    }
 
-        liste.setSelection(adapter.getCurrentSelection());
+    /**
+     * Load talks asynchronously
+     */
+    private class Loading extends AsyncTask<Void, Void, List<Talk>> {
+
+        private final Context context;
+
+        private Loading(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected List<Talk> doInBackground(Void... voids) {
+            return ConferenceFacade.getInstance().getWorkshopsAndTalks(context);
+        }
+
+        @Override
+        protected void onPostExecute(List<Talk> talks) {
+            ListTalkForFilAdapter adapter = new ListTalkForFilAdapter(context, talks);
+            talksListView.setAdapter(adapter);
+            talksListView.setSelection(adapter.getCurrentSelection());
+        }
     }
 }
